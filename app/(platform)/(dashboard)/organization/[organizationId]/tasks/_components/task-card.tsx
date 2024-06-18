8affa6b09e3394ef09e2f209eadbccd824ee912b
Philani Ncube <ncbphi001@gmail.com>
2024-06-18 22:29:42 +0200

    chore: Update task-card component to include task cost and date information

--- app/(platform)/(dashboard)/organization/[organizationId]/tasks/_components/task-card.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/tasks/_components/task-card.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/card";
 import type { Project, Task } from "@prisma/client";
 import { format } from "date-fns";
-import { Calendar } from "lucide-react";
+import { Calendar, CalendarCheck } from "lucide-react";
 import ProjectStatus from "../../projects/[projectId]/_components/project-status";
 import { cn, formatCurrency } from "@/lib/utils";
 import { useSortable } from "@dnd-kit/sortable";
@@ -33,37 +33,36 @@ const TaskCard = ({ task, index }: TaskCardProps) => {
 		<Draggable draggableId={task.id} index={index} >
 			{(provided) => {
 				return (
-					<Card className="my-2" {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
-						<CardHeader
-              {...provided.dragHandleProps}
-							className={cn(
-								"mb-2",
-								task.status === "completed" && "bg-green-600 text-white",
-								task.status === "notStarted" && "bg-zinc-200 text-slate-700",
-								task.status === "onHold" && "bg-yellow-100 text-zinc-600",
-								task.status === "inProgress" && "bg-blue-500 text-blue-50",
-							)}
-						>
-							<div className="flex justify-between">
-								<p className="text-xs">Project: {task.project.title}</p>
-							</div>
-
-							<h2 className="text-lg">{task.title}</h2>
-						</CardHeader>
+					<Card
+						className={cn(
+							"my-2 py-3 relative",
+							task.status === "completed" && "bg-green-600 text-white",
+							task.status === "notStarted" && "bg-zinc-200 text-slate-700",
+							task.status === "onHold" && "bg-yellow-100 text-zinc-600",
+							task.status === "inProgress" && "bg-blue-500 text-blue-50",
+						)}
+						{...provided.dragHandleProps}
+						{...provided.draggableProps}
+						ref={provided.innerRef}
+						{...provided.dragHandleProps}
+					>
 						<CardContent>
-							<div className="text-xs text-slate-600">
-								<p>{formatCurrency(task.cost)}</p>
+							<div className="flex items-start justify-end mb-4">
+								<div className="flex text-xs">
+									<CalendarCheck size={16} />
+									<span className="flex items-center space-x-2">
+										{format(task.startDate, "dd MMM")}
+									</span>{" "}
+									-{" "}
+									<span className="flex items-center space-x-2">
+										{task.dueDate && format(task?.dueDate, "dd MMM")}
+									</span>
+								</div>
 							</div>
+							<p className="mb-1 text-xs">Project: {task.project.title}</p>
+							<h2 className="text-lg leading-5">{task.title}</h2>
+							<p className="p-2 mt-2 text-xs text-white bg-black rounded-sm w-fit">{formatCurrency(task.cost)}</p>
 						</CardContent>
-						<CardFooter className="flex justify-start">
-							<span className="flex items-center space-x-2">
-								{format(task.startDate, "dd MMM")}
-							</span>{" "}
-							-{" "}
-							<span className="flex items-center space-x-2">
-								{task.dueDate && format(task?.dueDate, "dd MMM")}
-							</span>
-						</CardFooter>
 					</Card>
 				);
 			}}
